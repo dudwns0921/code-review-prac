ef3174acd12ca9311dacffea646dbd7a17bc3ec7
eliojung <elio.jung@obigo.com>
2025-10-27 11:14:00 +0900

    fix: 스위치 컴포넌트 구현

--- src/components/SwitchComponent.tsx
+++ src/components/SwitchComponent.tsx
@@ -1,6 +1,7 @@
 const SwitchComponent = () => {
     // Component logic goes here
     // For demonstration, we'll just return a simple div
+    // review required
     return <div>Switch Component</div>
 }
 
