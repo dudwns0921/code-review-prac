c2d5d3ef90a7c5d86a0230abb1ab6019eac40429
eliojung <elio.jung@obigo.com>
2025-10-27 11:18:15 +0900

    fix: 스위치 컴포넌트 구현

--- src/components/SwitchComponent.tsx
+++ src/components/SwitchComponent.tsx
@@ -2,6 +2,7 @@ const SwitchComponent = () => {
     // Component logic goes here
     // For demonstration, we'll just return a simple div
     // review required
+    // 프롭스로 전달된 case와 리액트 컴포넌트에 따라 다른 UI를 렌더링하는 스위치 컴포넌트
     return <div>Switch Component</div>
 }
 
